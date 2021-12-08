b268ca0478cf46d6622556a10ab863605ec44715
kahseng <S10208180@connect.np.edu.sg>
2021-12-09 02:53:35 +0800

    Change index redirect

---
+++ src/public/js/index.js
@@ -0,0 +1,107 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.0/firebase-app.js";
+import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.6.0/firebase-auth.js";
+import { getDatabase, ref, onValue } from "https://www.gstatic.com/firebasejs/9.6.0/firebase-database.js";
+
+let isDown = false, startX, scrollLeft;
+
+function handleDown(e) {
+    isDown = true;
+    $(this).addClass("active");
+    startX = e.pageX - $(this).offset().left;
+    scrollLeft = $(this).scrollLeft();
+}
+
+function handleLeave() {
+    isDown = false;
+    $(this).removeClass("active");
+}
+
+function handleUp() {
+    isDown = false;
+    $(this).removeClass("active");
+}
+
+function handleMove(e) {
+    if(!isDown) return;
+    e.preventDefault();
+    const x = e.pageX - $(this).offset().left;
+    const walk = (x - startX) * 2;
+    $(this).scrollLeft(scrollLeft - walk);
+}
+
+$(document).ready(() => {
+    // if section has slide class, the user can drag to scroll in the section
+    $(".slide").on({
+        "mousedown": handleDown,
+        "mouseleave": handleLeave,
+        "mouseup": handleUp,
+        "mousemove": handleMove
+    })
+    
+    const firebaseConfig = {
+        apiKey: "AIzaSyCeWQCzSS4ZkNO6sjhkxhe1jSMfpzdf_lE",
+        authDomain: "thrive-chart.firebaseapp.com",
+        databaseURL: "https://thrive-chart-default-rtdb.asia-southeast1.firebasedatabase.app",
+        projectId: "thrive-chart",
+        storageBucket: "thrive-chart.appspot.com",
+        messagingSenderId: "667460322691",
+        appId: "1:667460322691:web:866629265a85e6ed6b22b6",
+        measurementId: "G-BPWB4MX81R"
+    };
+    
+    const app = initializeApp(firebaseConfig);
+    const auth = getAuth();
+    const database = getDatabase(app);
+
+    onAuthStateChanged(auth, (user) => {
+        if (user) {
+            $("#nav-account").html(`
+                <li class="nav-item">
+                    <a class="nav-link" href="#">
+                        Hi <span id="nav-account-username"></span>
+                    </a>
+                </li>
+                <li class="nav-item pe-3">
+                    <button id="sign-out" class="btn btn-outline-light">
+                        <svg xmlns="http://www.w3.org/2000/svg" style="width: 1.5rem; height: 1.5rem;" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
+                        </svg>
+                        Sign Out
+                    </button>
+                </li>
+            `)
+            
+            const uid = user.uid;
+            let usernameRef = ref(database, 'users/' + uid + '/username')
+            onValue(usernameRef, (snapshot) => {
+                const data = snapshot.val()
+                $("#nav-account-username").text(data)
+            })
+
+            $("#sign-out").on("click", () => {
+                auth.signOut();
+            })
+        } 
+        
+        else {
+            $("#nav-account").html(`
+                <li class="nav-item">
+                    <a class="nav-link" href="./src/public/signup.html">
+                        <svg xmlns="http://www.w3.org/2000/svg" style="width: 1.5rem; height: 1.5rem;" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
+                        </svg>
+                        <span>Sign Up</span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                        <a class="nav-link" href="./src/public/login.html">
+                            <svg xmlns="http://www.w3.org/2000/svg" style="width: 1.5rem; height: 1.5rem;" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1" />
+                            </svg>
+                            <span>Login</span>
+                        </a>
+                </li>
+            `)
+        }
+    })
+})
