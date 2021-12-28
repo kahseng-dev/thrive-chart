4e854ea2b7a12477a2d1093f1ee4c087c877368a
kahseng <S10208180@connect.np.edu.sg>
2021-12-29 02:59:29 +0800

    Adjust mobile responsiveness

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -86,7 +86,7 @@ $(document).ready(() => {
         
         else {
             $("#nav-account").html(`
-                <li class="nav-item">
+                <li class="nav-item me-2">
                     <a class="nav-link" href="./src/public/signup.html">
                         <svg xmlns="http://www.w3.org/2000/svg" style="width: 1.5rem; height: 1.5rem;" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
@@ -94,7 +94,7 @@ $(document).ready(() => {
                         <span>Sign Up</span>
                     </a>
                 </li>
-                <li class="nav-item">
+                <li class="nav-item me-3">
                         <a class="nav-link" href="./src/public/login.html">
                             <svg xmlns="http://www.w3.org/2000/svg" style="width: 1.5rem; height: 1.5rem;" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1" />
